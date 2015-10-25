696fd057faac399122364806adbdd177252f7fd5
Niklas Nihlén <niklas.nihlen@acast.com>
2015-10-25 14:31:25 +0100

    Moved around config values a bit

---
+++ config.js
@@ -0,0 +1,12 @@
+var keys = require('./keys');
+
+module.exports = {
+	realtimedepatures: {
+		api_key: keys.realtimeinfo,
+		siteid: '1868',
+		timewindow: '20'
+	},
+	station_search: {
+		api_key: keys.station_search
+	}
+};
