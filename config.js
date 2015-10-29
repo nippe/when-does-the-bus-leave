79f12a50c1e842f20fe21b5203854b387c5880dd
Niklas Nihlén <niklas.nihlen@acast.com>
2015-10-29 08:18:34 +0100

    Removed fnutts to get numbers as numbers

--- config.js
+++ config.js
@@ -3,9 +3,9 @@ var keys = require('./keys');
 module.exports = {
 	realtimedepatures: {
 		api_key: keys.realtimeinfo,
-		siteid: '1868',
-		timewindow: '20',
-		direction: '2'
+		siteid: 1868,
+		timewindow: 20,
+		direction: 2
 	},
 	station_search: {
 		api_key: keys.sites
