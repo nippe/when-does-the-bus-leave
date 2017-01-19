1d86a0c2bbdfd41b44a4198fdc5bec616b850cb7
Niklas Nihlén <niklas.nihlen@gmail.com>
2017-01-19 23:03:45 +0100

    Changing API URL because of deprication
    Don't forget to update the key also.

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ var keys = require('./keys');
 module.exports = {
 	realtimedepatures: {
 		api_key: keys.realtimeinfo,
-		url: 'http://api.sl.se/api2/realtimedepartures.json',
+		url: 'http://api.sl.se/api2/realtimedeparturesV4.json',
 		siteid: 1868,
 		timewindow: 20,
 		direction: 2
